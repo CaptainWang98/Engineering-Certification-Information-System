691a00142268ebb167105c2b830f7228d7c48ab9
1291945816 <mac1291945816@163.com>
2020-12-03 21:37:31 +0800

    更新后台api

--- vue.config.js
+++ vue.config.js
@@ -27,7 +27,7 @@ module.exports = {
   publicPath: './',
   outputDir: 'dist',
   assetsDir: 'static',
-  lintOnSave: false, //process.env.NODE_ENV === 'development',
+  lintOnSave: false, // process.env.NODE_ENV === 'development',
   productionSourceMap: false,
   devServer: {
     port: port,
@@ -35,16 +35,8 @@ module.exports = {
     overlay: {
       warnings: false,
       errors: true
-    },
-    proxy: {
-      '/api': {
-        target: 'http://120.79.48.19',
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api': ''
-        }
-      }
     }
+
     // before: require('./mock/mock-server.js')
   },
   configureWebpack: {
